3b5d02a664af4e3829b5779fa87110982589e72c
gudkovst <69453922+gudkovst@users.noreply.github.com>
2022-11-05 21:09:30 +0800

    lab3
    Signed-off-by: gudkovst <69453922+gudkovst@users.noreply.github.com>

--- lab3/src/main/java/Keys.java
+++
@@ -1,7 +0,0 @@
-package config;
-
-public class Keys {
-    public static String KEY_PLACES = "5ae2e3f221c38a28845f05b63afc815c37a75f493e5de183c9bad3aa";
-    public static String KEY_GEOCODE = "8e6b5817-973a-49d7-83ab-05ed63e961b6";
-    public static String KEY_WEATHER = "c1043b3f585d80539d0f81888ed526f4";
-}
---
+++ lab3/src/main/java/config/Keys.java
@@ -0,0 +1,9 @@
+package config;
+
+//enter your own keys
+
+public class Keys { 
+    public static String KEY_PLACES = "";
+    public static String KEY_GEOCODE = "";
+    public static String KEY_WEATHER = "";
+}
