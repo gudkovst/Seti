f16ee9fdf6dbd02c365fb8ac9a58333b364b6458
gudkovst <69453922+gudkovst@users.noreply.github.com>
2022-11-05 21:09:54 +0800

    lab3
    Signed-off-by: gudkovst <69453922+gudkovst@users.noreply.github.com>

---
+++ lab3/src/main/java/config/Config.java
@@ -0,0 +1,8 @@
+package config;
+
+public class Config {
+    public static int WIDTH = 1000;
+    public static int HEIGHT = 500;
+    public static double RADIUS = 1000;
+    public static int MAX_RECORDS = 15;
+}
