94ba73afd039aab2d50c4aed1b3faeb1800e6d79
gudkovst <69453922+gudkovst@users.noreply.github.com>
2022-11-06 15:50:21 +0800

    fix lab3
    Signed-off-by: gudkovst <69453922+gudkovst@users.noreply.github.com>

--- lab3/src/main/java/records/Weather.java
+++ lab3/src/main/java/records/Weather.java
@@ -2,18 +2,12 @@ package records;
 
 public class Weather {
     private final String weather;
-    private final String description;
 
-    public Weather(String weather, String description){
+    public Weather(String weather){
         this.weather = weather;
-        this.description = description;
     }
 
     public String getWeather() {
         return weather;
-    }
-
-    public String getDescription() {
-        return description;
     }
 }
